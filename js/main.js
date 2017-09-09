295889e9f0e7cae50bffeee1925822452b6c99e8
Cameron Castells <cam@Camerons-MBP.hsd1.co.comcast.net>
2017-09-09 17:54:33 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -33,18 +33,29 @@ var checkForMatch = function(){
 		}
 }
 
-var flipCards = function (cardId){
+var flipCard = function (){
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute("src", cards[cardId].cardImage);
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 	}
 }
 
-flipCards(0);
-flipCards(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+createBoard();
 
 
 
